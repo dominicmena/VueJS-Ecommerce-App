083be0692d2cd7e79e235a13ac0ee497c404cdb2
dominicmena <mena.dominic@gmail.com>
2023-06-08 23:41:16 -0500

    notfoundpage added and tested

--- fsv-front-end/src/router/index.js
+++ fsv-front-end/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import CartPage from '../views/CartPage.vue'
 import ProductDetailPage from '../views/ProductDetailPage.vue'
 import ProductsPage from '../views/ProductsPage.vue'
+import NotFoundPage from '../views/NotFoundPage'
 
 
 Vue.use(VueRouter)
@@ -23,6 +24,9 @@ const routes = [
 }, {
   path: '/',
   redirect: '/products'
+}, {
+  path: '*',
+  component: NotFoundPage
 }
 ]
 
