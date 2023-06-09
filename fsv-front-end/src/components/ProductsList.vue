a4aba21c8a2aca50636e3a2c20f138f2ec9125fd
dominicmena <mena.dominic@gmail.com>
2023-06-09 00:09:28 -0500

    successfully rendered products in cart with reusable components

---
+++ fsv-front-end/src/components/ProductsList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="products.length > 0">
+    <ProductsListItem
+    v-for="product in products"
+    :key="product.id"
+    :product="product"/>
+</div>
+<p v-else>Your cart is empty</p>
+</template>
+
+<script>
+import ProductsListItem from './ProductsListItem.vue'
+export default {
+    name: 'ProductsList',
+    props: ['products'],
+    components: {
+        ProductsListItem,
+    },
+}
+</script>
