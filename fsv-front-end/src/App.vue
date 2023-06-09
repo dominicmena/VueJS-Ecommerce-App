8f83a7906f61b763169985b5a1492506dea9b5e0
dominicmena <mena.dominic@gmail.com>
2023-06-08 22:28:05 -0500

    added images

--- fsv-front-end/src/App.vue
+++ fsv-front-end/src/App.vue
@@ -4,7 +4,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view/> 
+    <!-- ^^^shows whatever is in the routes index.js -->
   </div>
 </template>
 
