62c42b7f8f2935f52cdd5b7f9bf2c73a611d723b
dominicmena <mena.dominic@gmail.com>
2023-06-08 22:20:59 -0500

    added all routes and created views

---
+++ fsv-front-end/src/views/CartPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>This is the Cart page</h1>
+    </template>
+    
+    <script>
+    export default {
+        name: 'CartPage',
+    }
+    </script>
+    
+    <style>
+    h1 {
+        color: green
+    }
+    </style>
