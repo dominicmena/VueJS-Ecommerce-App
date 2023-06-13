3ca309aea5eeecdfb55119fed57b1acfdffcb7ef
dominicmena <mena.dominic@gmail.com>
2023-06-12 21:06:09 -0500

    full crud achieved

--- fsv-front-end/src/views/CartPage.vue
+++ fsv-front-end/src/views/CartPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="page-wrap">
     <h1>Shopping Cart</h1>
-    <ProductsList :products="cartItems"/>
+    <ProductsList 
+    :products="cartItems"
+    v-on:remove-from-cart="removeFromCart($event)"
+    />
     <h3 id="total-price">Total: ${{ totalPrice }}</h3>
     <button id="checkout-button">Proceed to Checkout</button>
   </div>
@@ -26,6 +29,12 @@ export default {
         return this.cartItems.reduce(
             (sum, item) => sum + Number(item.price),0
         )
+    }
+  },
+  methods: {
+    async removeFromCart(productId) {
+      const result = await axios.delete(`/api/users/12345/cart/${productId}`)
+      this.cartItems = result.data
     }
   },
   async created() {
