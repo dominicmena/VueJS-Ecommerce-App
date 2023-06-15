2e78382052eb1e08f0fc30254258fbec302deec4
dominicmena <mena.dominic@gmail.com>
2023-06-15 15:53:32 -0500

    added error handling

--- fsv-front-end/src/views/ProductsPage.vue
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -21,7 +21,8 @@ export default {
       const response = await axios.get('/api/products');
       this.products = response.data;
     } catch (error) {
-      console.error("Error retrieving products:", error.response);
+      console.error("Error retrieving products:", error.response.data);
+      console.log("Error details:", error.response);
       // You can further handle the error here, such as showing an error message or taking appropriate action
     }
   },
